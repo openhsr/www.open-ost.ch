b2b266dea4cd65a346347cc9dee40a085c4a5a1d
omega-800 <georgiy.shevoroshkin@ost.ch>
2026-09-11 21:20:03 +0200

    flake dev setup additions

--- flake.nix
+++ flake.nix
@@ -5,23 +5,48 @@
   outputs =
     { self, nixpkgs }:
     let
-      system = "x86_64-linux";
-      pkgs = import nixpkgs {
-        inherit system;
-      };
+      systems = nixpkgs.lib.platforms.unix;
+      eachSystem =
+        f:
+        nixpkgs.lib.genAttrs systems (
+          system:
+          f (
+            import nixpkgs {
+              inherit system;
+              config = { };
+              overlays = [ ];
+            }
+          )
+        );
     in
     {
-      devShells.${system} = rec {
+      devShells = eachSystem (pkgs: {
         default = pkgs.mkShell {
           packages = with pkgs; [
             ruby
+            rubyPackages.github-pages
+            gnumake
           ];
-          shellHook = ''
-            echo ""
+        };
+      });
+
+      packages = eachSystem (pkgs: {
+        default = pkgs.writeShellApplication {
+          name = "develop-local";
+          runtimeInputs = with pkgs; [ ruby ];
+          text = ''
             bundle # same as `bundle install`
             bundle exec jekyll serve
           '';
         };
-      };
+      });
+
+      apps = eachSystem (
+        pkgs:
+        pkgs.lib.mapAttrs (_: drv: {
+          type = "app";
+          program = "${drv}${drv.passthru.exePath or "/bin/${drv.pname or drv.name}"}";
+        }) self.packages.${pkgs.stdenv.hostPlatform.system}
+      );
     };
 }
